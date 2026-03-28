83e008421edcf4c9e3ebb2b5214966cd70cc8761
Dhruv Jain (Bafna) <jdhruv282@gmail.com>
2026-03-28 16:38:37 +0530

    CI Pipeline Implemented

---
+++ _test.py
@@ -0,0 +1,43 @@
+import pytest
+
+def square(n):
+    return n ** 2
+
+def cube(n):
+    return n** 3
+
+def power(n):
+    return n** 5
+
+
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+
+
+def test_cube():
+    assert cube(2) == 8
+    assert cube(3) == 27
+
+
+def test_power():
+    assert power(2) == 32
+    assert power(3) == 243
+
+
+def test_invalid_input():
+    with pytest.raises(typeError):
+        square("string")
+
+
+
+
+
+
+
+
+
+
+
+
